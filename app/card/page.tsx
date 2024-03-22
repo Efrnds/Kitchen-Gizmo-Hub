7029de98449ea90f362f297c5bfdfb21cff4755b
Eduardo fernandes da silva <eduardo.fernandes.silva13@gmail.com>
2024-03-22 14:37:01 -0300

    daazsdf

--- app/card/page.tsx
+++ app/card/page.tsx
@@ -1,21 +1,31 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Card({ title, price, imageUrl, imageAlt }) {
-    return (
-        <Link href="" className="p-4 w-96 h-2/3">
-            <Image
-                className="w-80 h-80"
-                src={imageUrl}
-                width={300}
-                height={300}
-                alt={imageAlt}
-            />
+interface CardProps {
+    title: string;
+    price: string;
+    imageUrl: string;
+    imageAlt: string;
+}
 
-            <div className="flex flex-col justify-start mt-2">
-                <h2 className="text-xl  text-stone-700">{title}</h2>
-                <p className="text-red-600 ">{price}</p>
-            </div>
+const Card: React.FC<CardProps> = ({ title, price, imageUrl, imageAlt }) => {
+    return (
+        <Link href="" passHref>
+            <a className="p-4 w-96 h-fit items-center flex flex-col rounded-sm shadow-sm bg-white">
+                <Image
+                    className="w-80 rounded-sm h-80"
+                    src={imageUrl}
+                    width={100}
+                    height={100}
+                    alt={imageAlt}
+                />
+                <div className="flex flex-col self-start px-4 mt-2">
+                    <h2 className="text-xl text-stone-700">{title}</h2>
+                    <p className="text-red-600">{price}</p>
+                </div>
+            </a>
         </Link>
     );
 }
+
+export default Card;
